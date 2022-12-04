ffb02abaf71bd12523a3f535765483264eddb711
Jose Lara de Lamo <joselaradelamo@gmail.com>
2022-12-04 22:53:48 +0100

    feat: add day4 code

---
+++ Day4/part2.py
@@ -0,0 +1,20 @@
+import openInput
+
+data = openInput.openFile()
+assignments = data.split("\n");
+total = 0;
+for value in assignments:
+  part1, part2 = value.split(',');
+  part1Start,part1End = part1.split('-')
+  part2Start,part2End = part2.split('-')
+  if int(part1Start) >= int(part2Start) and int(part1Start) <= int(part2End):
+    total = total + 1;
+  elif int(part1End) >= int(part2Start) and int(part1End) <= int(part2End):
+    total = total + 1;
+  elif int(part2Start) >= int(part1Start) and int(part2End) <= int(part1End):
+    total = total + 1;
+  elif int(part2End) >= int(part1Start) and int(part2End) <= int(part1End):
+    total = total + 1;
+  
+  
+print(total)
