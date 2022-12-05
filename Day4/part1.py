6f4c5cff253a746265a0778a89b2c1e10e73ae68
Jose Lara de Lamo <joselaradelamo@gmail.com>
2022-12-05 22:40:35 +0100

    feat: Fixed day 4

--- Day4/part1.py
+++ Day4/part1.py
@@ -1,15 +1,15 @@
 import openInput
 
 data = openInput.openFile()
-assignments = data.split("\n");
-total = 0;
+assignments = data.split("\n")
+total = 0
 for value in assignments:
-  part1, part2 = value.split(',');
+  part1, part2 = value.split(',')
   part1Start,part1End = part1.split('-')
   part2Start,part2End = part2.split('-')
   if int(part1Start) <= int(part2Start) and int(part1End) >= int(part2End):
-    total = total + 1;
+    total += 1
   elif int(part1Start) >= int(part2Start) and int(part1End) <= int(part2End):
-    total = total + 1;
+    total += 1
   
 print(total)
