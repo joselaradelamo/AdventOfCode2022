d2585c56f6f0ce82eb0f8ef521f644de34737c93
Jose Lara de Lamo <joselaradelamo@gmail.com>
2022-12-09 17:17:11 +0100

    Update test_Day1.py

--- test/unit/test_Day1.py
+++ test/unit/test_Day1.py
@@ -1,8 +1,8 @@
 
 import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir1 = os.path.dirname(parentdir1)
-parentdir = os.path.dirname(currentdir)
+parentdir1 = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir1)
 sys.path.append(parentdir)
 
 import unittest
