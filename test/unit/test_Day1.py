b9a131931188ad71a0664cdc8b1810232580ac00
Jose Lara de Lamo <joselaradelamo@gmail.com>
2022-12-09 17:32:30 +0100

    fix: small fixes

--- test/unit/test_Day1.py
+++ test/unit/test_Day1.py
@@ -1,10 +1,3 @@
-
-import os, sys
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir1 = os.path.dirname(currentdir)
-parentdir = os.path.dirname(parentdir1)
-sys.path.append(parentdir)
-
 import unittest
 from Day1.index import part1,part2
 from helpers import getExample,getInput
